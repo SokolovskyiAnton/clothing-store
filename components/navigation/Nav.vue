5ac550646a9ec92f1305d48468f9874399e2acdb
Your Name <you@example.com>
2021-07-11 02:30:14 +0300

    Сделана структура навигации

---
+++ components/navigation/Nav.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="nav-shop">
+        <div class="nav-header">
+          <div class="nav-header__logo">
+            <div>Astore</div>
+          </div>
+          <div class="nav-header__center">
+            <ul>
+              <li>
+                <nuxt-link to="/men">Men</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="/women">Women</nuxt-link>
+              </li>
+            </ul>
+
+            <form @submit.prevent="" class="d-flex align-items-center nav-header-form__container">
+              <span><font-awesome-icon class="fa" icon="search" /></span>
+              <input class="nav-header-form__search me-2" type="search" >
+            </form>
+          </div>
+        </div>
+        <div class="nav-footer"></div>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+@import '@/assets/style/global.scss';
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,23 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="app">
+    <Navbar/>
+    <div class="content">
+      <main>
+        <Nuxt />
+      </main>
+    </div>
   </div>
 </template>
-
+<script>
+export default {
+  name: 'Default',
+  components: {
+    Navbar: () => import('@/components/navigation/Nav')
+  }
+}
+</script>
 <style>
+.app {
+  min-height: 100vh;
+}
 </style>
